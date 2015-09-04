237a575d2ba1c682cbce439d9806155fc0043020
Glenn Eggleton <geggleto@gmail.com>
2015-09-04 15:05:02 -0400

    Adding more tabs to satisfy my OCD

--- src/routes.php
+++ src/routes.php
@@ -10,11 +10,11 @@ use Blog\Blog\BlogController;
 use Blog\Middleware\SecurityMiddleware;
 
 
-$app->get(      '/',            BlogController::class . ":homePage");
+$app->get(      '/',                BlogController::class . ":homePage");
 $app->put(      '/blog/{id:\d+}',   BlogController::class . ":update")->addMiddleware(SecurityMiddleware::class);
-$app->post(     '/blog',        BlogController::class . ":create")->addMiddleware(SecurityMiddleware::class);
+$app->post(     '/blog',            BlogController::class . ":create")->addMiddleware(SecurityMiddleware::class);
 $app->get(      '/blog/{id:\d+}',   BlogController::class . ":get")->addMiddleware(SecurityMiddleware::class);
 $app->delete(   '/blog/{id:\d+}',   BlogController::class . ":remove")->addMiddleware(SecurityMiddleware::class);
 
 //setup db tables
-$app->get(   '/blog/migrate',   BlogController::class . ":migrate")->addMiddleware(SecurityMiddleware::class);
+$app->get(      '/blog/migrate',    BlogController::class . ":migrate")->addMiddleware(SecurityMiddleware::class);
